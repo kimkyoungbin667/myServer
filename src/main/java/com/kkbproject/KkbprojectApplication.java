522d38fee6a74af6abf18f2360b72d17972513a3
kkb <rudqls1144@naver.com>
2024-12-06 20:15:26 +0900

    초기설정

---
+++ src/main/java/com/kkbproject/KkbprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kkbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkbprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkbprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkbproject/KkbprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkbprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
